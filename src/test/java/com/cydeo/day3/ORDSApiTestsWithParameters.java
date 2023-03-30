646e5c7c5bd931664d40b5a14fc2325c88fc5865
sultanmercan <sultanmercann@gmail.com>
2023-03-30 20:08:10 +0200

    Day3 ORDSApi class added

---
+++ src/test/java/com/cydeo/day3/ORDSApiTestsWithParameters.java
@@ -0,0 +1,68 @@
+package com.cydeo.day3;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class ORDSApiTestsWithParameters {
+
+     /*
+        Given accept type is Json
+        And parameters: q = {"region_id":2}
+        When users sends a GET request to "/countries"
+        Then status code is 200
+        And Content type is application/json
+        And Payload should contain "United States of America"
+     */
+
+    @BeforeAll
+    public static void init (){
+
+        RestAssured.baseURI = "http://100.25.147.218:1000/ords/hr";
+    }
+    @DisplayName("GET request to /countries with Query Param")
+    @Test
+    public void test1(){
+
+        Response response = given().accept(ContentType.JSON).and()
+                .queryParam("q", "{\"region_id\":2}")
+                .when().get("/countries");
+
+        assertEquals(200, response.statusCode());
+
+        assertEquals("application/json", response.contentType());
+
+        assertTrue(response.body().asString().contains("United States of America"));
+
+    }
+
+     /*
+        Send a GET request to employees and get only employees who works as a IT_PROG
+
+     */
+
+    @DisplayName("GET request to /employees with Query Param")
+    @Test
+    public void test2(){
+
+        Response response = given().accept(ContentType.JSON).and().
+                queryParam("q", "{\"job_id\": \"IT_PROG\"}")
+                .when().get("/employees");
+
+        assertEquals(200,response.statusCode());
+
+        assertEquals("application/json", response.contentType());
+
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+
+    }
+
+
+
+}
