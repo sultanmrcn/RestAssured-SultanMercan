6dfa33d5080631c5a8c3dc8bcd86afe0a5f22036
sultanmercan <sultanmercann@gmail.com>
2023-04-03 19:10:10 +0200

    ORDSApiWithJsonPath

---
+++ src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
@@ -0,0 +1,25 @@
+package com.cydeo.day4;
+
+import com.cydeo.utilities.HrTestBase;
+import com.cydeo.utilities.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ORDSApiWithJsonPath extends HrTestBase {
+    @DisplayName("GET request to Countries")
+    @Test
+    public void test1(){
+
+
+
+
+    }
+}
