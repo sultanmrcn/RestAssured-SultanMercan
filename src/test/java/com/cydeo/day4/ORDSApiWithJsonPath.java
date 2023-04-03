b3f48af3616f7569a6255956efa8cc4347f2b779
sultanmercan <sultanmercann@gmail.com>
2023-04-03 20:51:28 +0200

    ORDSApiWithJsonPath added

--- src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
@@ -3,6 +3,7 @@ package com.cydeo.day4;
 import com.cydeo.utilities.HrTestBase;
 import com.cydeo.utilities.HrTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,12 +15,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ORDSApiWithJsonPath extends HrTestBase {
+
+
     @DisplayName("GET request to Countries")
     @Test
     public void test1(){
 
+        Response response = get("/countries");
 
+        //get the second country name with JsonPath
 
+        JsonPath jsonPath = response.jsonPath();
+
+        String secondCountryName = jsonPath.getString("items[1].country_name");
+        System.out.println("secondCountryName = " + secondCountryName);
+
+        //get all country ids
+        List<String > allCountryIds = jsonPath.getList("items.country_id");
+        System.out.println("allCountryIds = " + allCountryIds);
+
+        //get all country names where their region id is equal to 2
+        //we implement filter json that we have
+        //how you navigate inside the json with an if statement (to filter)--> "items.findAll {it.region_id==2}.country_name"
+        //   it:each
+        //   check each of the region id
+        //   and when the region id is 2, get the country name(value) in return
+
+        List<String> countryNameWithRegionId2 = jsonPath.getList("items.findAll{it.region_id==2}.country_name");
+        System.out.println("countryNameWithRegionId2 = " + countryNameWithRegionId2);
 
     }
 }
