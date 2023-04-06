ec028a33041de0aa8f381811164cb40249850004
sultanmercan <sultanmercann@gmail.com>
2023-04-06 19:44:52 +0200

    day5 ORDS Hamcrest Test -extract

--- src/test/java/com/cydeo/day5/ORDSHamcrestTest.java
+++ src/test/java/com/cydeo/day5/ORDSHamcrestTest.java
@@ -12,16 +12,16 @@ import static org.hamcrest.Matchers.*;
 
 public class ORDSHamcrestTest extends HrTestBase {
 
-
-
     @DisplayName("GET request to Employees IT_PROG endpoint and chaining")
     @Test
     public void employeesTest(){
+
         //send a get request to emplyoees endpoint with query parameter job_id IT_PROG
         //verify each job_id is IT_PROG
         //verify first names are .... (find proper method to check list against list)
         //verify emails without checking order (provide emails in different order,just make sure it has same emails)
         //expected names
+        //
 
         List<String> names =Arrays.asList("Alexander","Bruce","David","Valli","Diana");
 
