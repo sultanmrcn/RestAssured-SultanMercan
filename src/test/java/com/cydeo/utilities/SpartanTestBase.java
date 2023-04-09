2f4ddc4ad115b096c574e238489a761711ec462b
sultanmercan <sultanmercann@gmail.com>
2023-04-09 15:39:44 +0200

    Uncomment DBUtils connection

--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -10,11 +10,11 @@ public class SpartanTestBase {
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
         baseURI = "http://100.25.147.218:8000";
-        String dbUrl = "jdbc:oracle:thin:@35.175.124.71:1521:xe";
+        String dbUrl = "jdbc:oracle:thin:@100.25.147.218:1521:xe";
         String dbUsername = "SP";
         String dbPassword = "SP";
 
-        //DBUtils.createConnection(dbUrl, dbUsername, dbPassword);
+        DBUtils.createConnection(dbUrl, dbUsername, dbPassword);
     }
     @AfterAll
     public static void teardown(){
