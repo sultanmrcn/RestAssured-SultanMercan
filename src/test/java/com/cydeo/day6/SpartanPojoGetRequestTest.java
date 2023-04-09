c0989958cce6ed611d58cbe63fb0f8544b69eca1
sultanmercan <sultanmercann@gmail.com>
2023-04-09 21:27:17 +0200

    Day6: SpartanPojoGetRequestTest.java added

--- src/test/java/com/cydeo/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cydeo/day6/SpartanPojoGetRequestTest.java
@@ -1,11 +1,16 @@
 package com.cydeo.day6;
 
+import com.cydeo.pojo.Search;
 import com.cydeo.pojo.Spartan;
 import com.cydeo.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -13,12 +18,12 @@ import static org.hamcrest.Matchers.*;
 public class SpartanPojoGetRequestTest extends SpartanTestBase {
     @DisplayName("GET one spartan and convert it to Spartan Object")
     @Test
-    public void oneSpartanPojo(){
+    public void oneSpartanPojo() {
         Response response = given().accept(ContentType.JSON)
                 .and().pathParam("id", 15)
                 .when().get("/api/spartans/{id}")
                 .then().statusCode(200)
-                .log().all()
+                //.log().all()
                 .extract().response();
         //Deserialize --> JSON to POJO (java custom class)
         //2 different way to do this
@@ -31,6 +36,65 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
         System.out.println("spartan15.getId() = " + spartan15.getId());
         System.out.println("spartan15.getGender() = " + spartan15.getGender());
 
+        //second way of deserialize json to java
+        //2.using JsonPath to deserialize to custom class
+
+        JsonPath jsonPath = response.jsonPath();
+        Spartan s15 = jsonPath.getObject("", Spartan.class);
+        System.out.println("s15.getName() = " + s15.getName());
+        System.out.println("s15.getPhone() = " + s15.getPhone());
 
     }
+
+    @DisplayName("Get one spartan from search endpoint result and use POJO")
+    @Test
+    public void spartanSearchWithPojo() {
+        ///spartans/search?nameContains=a&gender=Male
+        // send get request to above endpoint and save first object with type Spartan POJO
+        JsonPath jsonPath = given().accept(ContentType.JSON)
+                .and().queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .extract().jsonPath();
+        //get the first spartan from content list and put inside spartan object
+        Spartan s1 = jsonPath.getObject("content[0]", Spartan.class);
+        System.out.println("s1 = " + s1);
+        System.out.println("s1.getName() = " + s1.getName());
+        System.out.println("s1.getGender() = " + s1.getGender());
+    }
+
+    @Test
+    public void test3() {
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .extract().response();
+
+        Search searchResult = response.as(Search.class);
+        System.out.println(searchResult.getContent().get(0).getName());
+        //                              List         first element first element's name
+    }
+
+    @DisplayName("GET  /spartans/search and save as List<Spartan>")
+    @Test
+    public void test4(){
+        List<Spartan> spartanList = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .extract().jsonPath().getList("content", Spartan.class);
+        //without creating search pojo class, you can use .extract().jsonPath().getList() and List<Spartan> spartanList
+
+        System.out.println(spartanList.get(1).getName());
+
+    }
+
 }
