752e710cdc6540168dc8c8ca03e11901a31348a8
sultanmercan <sultanmercann@gmail.com>
2023-04-09 20:12:52 +0200

    Day6: Spartan.java added

---
+++ src/test/java/com/cydeo/day6/SpartanPojoGetRequestTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.day6;
+
+import com.cydeo.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanPojoGetRequestTest extends SpartanTestBase {
+    @DisplayName("GET one spartan and convert it to Spartan Object")
+    @Test
+    public void oneSpartanPojo(){
+
+
+    }
+}
