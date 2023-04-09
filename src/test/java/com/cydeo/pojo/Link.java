b46b6695f68655ef1a6fa01d64db8498b661b6e8
sultanmercan <sultanmercann@gmail.com>
2023-04-09 21:58:56 +0200

    Day6: Link

--- src/test/java/com/cydeo/pojo/Link.java
+++ src/test/java/com/cydeo/pojo/Link.java
@@ -8,10 +8,33 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Link {
-    private String rel;
+public class Link{
+    private  String rel;
     private String href;
 
+    public String getRel() {
+        return rel;
+    }
 
+    public void setRel(String rel) {
+        this.rel = rel;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{" +
+                "rel='" + rel + '\'' +
+                ", href='" + href + '\'' +
+                '}';
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+
+    }
 
 }
