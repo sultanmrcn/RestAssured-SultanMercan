40de79b49d02e87fa8dc7b39beb8d24479c08c41
sultanmercan <sultanmercann@gmail.com>
2023-04-10 15:38:15 +0200

    Day6:regionPojoTest added

--- src/test/java/com/cydeo/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cydeo/day6/ORDSPojoGetRequestTest.java
@@ -34,10 +34,12 @@ public class ORDSPojoGetRequestTest extends HrTestBase {
      */
     @Test
     public void regionTest(){
-        JsonPath jsonPath = get("/regions").then().statusCode(200)
-                .log().body().extract().jsonPath();
+
+        JsonPath jsonPath = get("/regions").then().statusCode(200).log().body().extract().jsonPath();
+
+        //pointing first region: item and item's first element
         Region region1 = jsonPath.getObject("items[0]", Region.class);
-        System.out.println("region1 = " + region1);
+        System.out.println(region1);
 
         System.out.println("region1.getRegion_id() = " + region1.getRId());
         System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
@@ -50,4 +52,70 @@ public class ORDSPojoGetRequestTest extends HrTestBase {
 
 
     }
+
+
+
+    @DisplayName("GET request to /employees and only get couple of values as a Pojo class")
+    @Test
+    public void employeeGet(){
+
+        Employee employee1 = get("/employees").then().statusCode(200)
+                .extract().jsonPath().getObject("items[0]", Employee.class);
+
+        System.out.println(employee1);
+
+//body nin sadece bir kısmını istiyorsak, diğer kalanı yoksaymalıyız.
+// bu yüzden pojo classta @JsonIgnoreProperties(ignoreUnknown = true) kullan.
+    }
+
+
+
+
+
+    /* send a get request to regions
+    verify that region ids are 1,2,3,4
+    verify that regions names Europe ,Americas , Asia, Middle East and Africa
+    verify that count is 4
+    try to use pojo as much as possible
+    ignore non used fields
+ */
+    @DisplayName("GET request to region only some fields test")
+    @Test
+    public void regionPojoTest() {
+        //send a get request and save everthing inside the regions object
+        //since we prepare pojo also for the all properties we dont need to use any path so as() method is enough
+        Regions regions = get("/regions").then().statusCode(200).extract().response().as(Regions.class);
+
+        //verify count is 4
+        assertThat(regions.getCount(),is(4));
+        //create empty list to store values
+        List<String> regionNames = new ArrayList<>();
+        List<Integer> regionIds = new ArrayList<>();
+
+        //get list of regions out of regions object
+        List<Region> items = regions.getItems();
+
+        //loop through each of the region, save their ids and names to empty list that we prepare
+        for (Region region : items) {
+            regionIds.add(region.getRId());
+            regionNames.add(region.getRegion_name());
+        }
+        System.out.println("regionIds = " + regionIds);
+        System.out.println("regionNames = " + regionNames);
+        //prepare expected result
+        List<Integer> expectedRegionIds = Arrays.asList(1,2,3,4);
+        List<String> expectedRegionNames = Arrays.asList("Europe", "Americas", "Asia", "Middle East and Africa");
+
+        //compare two result
+        assertThat(regionIds,is(expectedRegionIds));
+        assertThat(regionNames,is(equalTo(expectedRegionNames)));
+    }
+
+
+
+
+
+
+
+
 }
