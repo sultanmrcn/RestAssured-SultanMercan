24a11463d5ccb920689e409b7fa0014ae6a52011
sultanmercan <sultanmercann@gmail.com>
2023-04-12 18:48:56 +0200

    link

--- src/test/java/com/cydeo/pojo/Link.java
+++ src/test/java/com/cydeo/pojo/Link.java
@@ -12,29 +12,4 @@ public class Link{
     private  String rel;
     private String href;
 
-    public String getRel() {
-        return rel;
-    }
-
-    public void setRel(String rel) {
-        this.rel = rel;
-    }
-
-    public String getHref() {
-        return href;
-    }
-
-    @Override
-    public String toString() {
-        return "Link{" +
-                "rel='" + rel + '\'' +
-                ", href='" + href + '\'' +
-                '}';
-    }
-
-    public void setHref(String href) {
-        this.href = href;
-
-    }
-
 }
